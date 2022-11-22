7510fd21bf45c1eb18a2803f305ff90b1e6c3408
emart-1-gbg <enes.mart.gbg22@amerikanskagymnasiet.se>
2022-11-22 10:59:37 +0100

    cart

--- cart-contents.js
+++ cart-contents.js
@@ -1,7 +1,12 @@
-const cartContent = [];
+let basket = [];
+
+sessionStorage.getItem('basket')
 
 let addToCart = (id) => {
     let selectedItem = id;
     console.log(selectedItem.id)
+    if (selectedItem.id == 'fps-1') {
+        console.log('succsess')
+    }
 }
 
